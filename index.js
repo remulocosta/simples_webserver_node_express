e0484b89c42a6f33a8fdd31e2e950b9bdcb40791
Remulo Costa <remulo.costa@gmail.com>
2020-03-03 04:45:44 -0300

    Create module Post

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser')
 
 const userRoute = require('./routes/userRoute');
+const postRoute = require('./routes/postRoute');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}))
 const port = 3000;
 
 userRoute(app)
+postRoute(app)
 
 app.get('/', (req, res) => res.send('Olá mundo, Express!'));
 
---
+++ routes/postRoute.js
@@ -0,0 +1,60 @@
+const fs = require('fs')
+const { join } = require('path')
+
+const filePath = join(__dirname, 'posts.json')
+
+const getPosts = () => {
+  const data = fs.existsSync(filePath) 
+        ? fs.readFileSync(filePath) 
+        : [];
+
+  try {
+    return JSON.parse(data)
+  } catch (error) {
+    return []
+  }
+}
+
+const savePost = (posts) => fs.writeFileSync(filePath, JSON.stringify(posts, null, '\t'))
+
+const postRoute = (app) => {
+  app.route('/posts/:id?')
+      .get((req, res) => {
+        const posts = getPosts()
+
+        res.send({ posts })
+      })
+      .post((req, res) => {
+          const posts = getPosts()
+
+          posts.push(req.body)
+          savePost(posts)
+
+          res.status(201).send('ok')
+      })
+      .put((req, res) => {
+        const posts = getPosts();
+
+        savePost(posts.map(post => {
+          if (post.id === req.params.id) {
+            return {
+              ...post,
+              ...req.body
+            }
+          }
+
+          return post
+        }))
+
+        res.status(200).send('ok')
+      })
+      .delete((req, res) => {
+        const posts = getPosts()
+
+        savePost(posts.filter(post => post.id !== req.params.id))
+
+        res.status(200).send('ok')
+      })
+}
+
+module.exports = postRoute
