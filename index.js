a73284d8baec6565d3bfb2334303b4165d0d8fca
Remulo Costa <remulo.costa@gmail.com>
2020-03-03 04:08:12 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const userRoute = require('./routes/userRoute');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}))
+const port = 3000;
+
+userRoute(app)
+
+app.get('/', (req, res) => res.send('Olá mundo, Express!'));
+
+app.listen(port, () => console.log('API rodando na porta', port));
